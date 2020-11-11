d5f13f7d39cc6ecf61ca93025fe9aec9113dd482
FuChai Mead <fuchaimead@FuChais-MacBook-Pro.local>
2020-11-10 21:55:56 -0700

    handle winner

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,5 @@
 import Details from './Details';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
@@ -9,12 +9,33 @@ const Board = () => {
     6: null, 7: null, 8: null
   }
   const [values, setValues] = useState(emptyGame);
-
   const [xIsNext, setXisNext] = useState(true);
+  const [winner, setWinner] = useState(false);
+  const [winnerName, setWinnerName] = useState('');
+
+  useEffect(() => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (values[a] && values[a] === values[b] && values[a] === values[c]) {
+        setWinner(true);
+        setWinnerName(values[a])
+      }
+    }
+  });
 
   const handleClick = (number) => {
     const letter = xIsNext ? 'X' : 'O';
-    if(values[number] === null) {
+    if(values[number] === null && !winner) {
       setValues({...values, [number]: letter});
       setXisNext(!xIsNext);
     }
@@ -28,7 +49,7 @@ const Board = () => {
   const renderSquare = (number) => {
     return (
       <div className='square' onClick={() => handleClick(number)}>
-      <Square value={values[number]} number={number}/>
+        <Square value={values[number]}/>
       </div>
     );
   }
@@ -37,6 +58,7 @@ const Board = () => {
     <>
     <button onClick={() => newGame()}>New Game</button>
     <div>
+    {winner && <div className='win'><strong>{winnerName} Wins!</strong></div>}
       <div className='row'>
         {renderSquare(0)}
         {renderSquare(1)}
--- src/Square.js
+++ src/Square.js
@@ -1,19 +1,9 @@
 import React, { useState } from 'react';
 
 const Square = (props) => {
-  const [value, setValue] = useState(null);
-  console.log('value', value)
-  const handleClick = () => {
-    if(value === null && value === 'X') {
-      setValue('X') 
-    } else {
-      setValue('O')
-    }
-  }
   return (
-  <div>{props.value}</div>
+    <div>{props.value}</div>
   )
-
 }
 
 export default Square;
